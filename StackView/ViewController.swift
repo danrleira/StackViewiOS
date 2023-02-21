80032eb08a617374900cf8d7267011e7a466e9ce
danrleira <danrleimark@gmail.com>
2023-02-21 20:25:39 -0300

    Initial Commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Danrlei Ribeiro Azevedo on 21/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
